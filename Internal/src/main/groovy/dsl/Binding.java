2b758822750acad43559ca04ccda68bffb1cc2b5
I576958 <vincent.boillot.devalliere@sap.com>
2023-11-27 10:37:27 +0100

    chore: kernels

--- Internal/src/main/groovy/dsl/Binding.java
+++
@@ -1,4 +0,0 @@
-package dsl;
-
-public class Binding {
-}
--- Internal/src/main/groovy/dsl/DSL.groovy
+++ Internal/src/main/groovy/dsl/DSL.groovy
@@ -1,2 +1,64 @@
 package dsl
 
+import org.codehaus.groovy.control.CompilerConfiguration
+import org.codehaus.groovy.control.customizers.SecureASTCustomizer
+
+class DSL {
+    private GroovyShell shell
+    private CompilerConfiguration configuration
+    private Binding binding
+    private Basescript basescript
+
+    DSL() {
+        binding = new Binding()
+        binding.setGroovuinoMLModel(new Model(binding));
+        configuration = getDSLConfiguration()
+        configuration.setScriptBaseClass("main.groovy.dsl.Basescript")
+        shell = new GroovyShell(configuration)
+
+        binding.setVariable("high", SIGNAL.HIGH)
+        binding.setVariable("low", SIGNAL.LOW)
+    }
+
+    private static CompilerConfiguration getDSLConfiguration() {
+        def secure = new SecureASTCustomizer()
+        secure.with {
+            //disallow closure creation
+            closuresAllowed = false
+            //disallow method definitions
+            methodDefinitionAllowed = true
+            //empty white list => forbid imports
+            importsWhitelist = [
+                    'java.lang.*'
+            ]
+            staticImportsWhitelist = []
+            staticStarImportsWhitelist= []
+            //language tokens disallowed
+//			tokensBlacklist= []
+            //language tokens allowed
+            tokensWhitelist= []
+            //types allowed to be used  (including primitive types)
+            constantTypesClassesWhiteList= [
+                    int, Integer, Number, Integer.TYPE, String, Object
+            ]
+            //classes who are allowed to be receivers of method calls
+            receiversClassesWhiteList= [
+                    int, Number, Integer, String, Object
+            ]
+        }
+
+        def configuration = new CompilerConfiguration()
+        configuration.addCompilationCustomizers(secure)
+
+        return configuration
+    }
+
+    void eval(File scriptFile) {
+        Script script = shell.parse(scriptFile)
+
+        binding.setScript(script)
+        script.setBinding(binding)
+
+        script.run()
+    }
+}
